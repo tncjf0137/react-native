766290375333482a6fc3f849e5cb86f28cf725d9
tncjf0137 <tncjf0137@gmail.com>
2020-08-25 13:31:39 +0900

    props

--- App.js
+++ App.js
@@ -1,107 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { Component } from 'react';
+import { StyleSheet, Button, View, Image, Text } from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+type Props = {};
+class Chopper extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={require(`./assets/c2.jpg`)} style={{ width: 100, height: 100 }}></Image>
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-       <View style={styles.container}>
-        <View style={styles.header} />
-        <View style={styles.content}>
-
-          <View style={styles.elem}>
-            <View style={styles.userInfo}>
-              <View style={styles.profile} />
-              <Text style={styles.name}>윾또막</Text>
-            </View>
-            <View style={styles.userComment}>
-              <Text>대화명을 입력하세요</Text>
-            </View>
-          </View>
-
-          <View style={styles.elem}>
-            <View style={styles.userInfo}>
-              <View style={styles.profile} />
-              <Text style={styles.name}>저커버그</Text>
-            </View>
-            <View style={styles.userComment}>
-              <Text>정말 맥주라도 한 잔 사는게 어떻겠니?</Text>
-            </View>
-          </View>
-
-        </View>
-        <View style={styles.footer} />
       </View>
-    </>
-  );
-};
+    )
+  }
+}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Chopper:yummy</Text>
+        <Chopper></Chopper>
+
+      </View >
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  header: {
-    height:60,
-    backgroundColor:'green',
-  },
-  footer: {
-    height:60,
-    backgroundColor:'red',
-  },
-  content: {
-    flex:1,
-  },
-
-  elem: {
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    borderColor:'#eee',
-    borderBottomWidth:0.5,
-    padding: 5,
-  },
-  userInfo: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  userComment: {
-    padding:8,
-    backgroundColor:'yellow',
-    borderRadius:5,
-  },
-  profile: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    backgroundColor: 'yellow',
-  },
-  name: {
-    paddingLeft: 10,
-  }
-  
 });
-
-export default App;
